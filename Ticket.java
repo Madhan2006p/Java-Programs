395bcc1a61dce44b1b09ef31812bd8c0cc068496
Madhan P <madhanp.23msc@kongu.edu>
2025-06-25 10:54:02 +0530

    Ticket ReCoded

--- Ticket.java
+++ Ticket.java
@@ -2,7 +2,11 @@ class Ticket {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
-        int TotalAdults = (x * 5) - 700 / (x * 5);
-        System.out.println(TotalAdults); 
+        int Child = (y - 5 * x) / 13;
+        int Adult = x + Child;
+        int Senior = Child * 2;
+        System.out.println(Child);
+        System.out.println(Adult);
+        System.out.println(Senior); 
     }
 }
