bc71f1c751a4e0571fa7326d3fc189a3b704fe5d
Madhan P <madhanp.23msc@kongu.edu>
2025-06-25 12:02:20 +0530

    test calculation Program was Added

---
+++ testCalculation.java
@@ -0,0 +1,23 @@
+class testCalculation {
+		
+		public static int Fact (int n) {
+			int f = 1;
+			for(int var = 1 ; var <= n ;  var += 1)
+					f = f * var;
+			return f;
+		}
+		public static void main (String[] args) { 
+			int n = Integer.parseInt(args[0]);
+			double sum = 0;
+			for(int var = 1 ; var <= n ; var += 1) 
+				sum = sum + (double) ((double)var / (double) Fact(var)) ;
+			
+			System.out.println(sum);
+		}
+	
+	
+	
+	
+	
+	
+}
